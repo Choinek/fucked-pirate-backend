e34ede1403ff0e7e303eec0fb14dceb859665b72
Choinek <adrian@chojnicki.pl>
2020-01-25 19:57:47 +0100

    + more frequency send data update to players

--- app/src/Server.php
+++ app/src/Server.php
@@ -169,7 +169,7 @@ class Server
         if (isset($data['gameserver'])) {
             self::setGameserver($frame);
             $server->push($frame->fd, json_encode(['s' => 100]));
-            swoole_timer_tick(80, function ($timerId) use ($server) {
+            swoole_timer_tick(20, function ($timerId) use ($server) {
                 $playersData = json_decode(self::$gameworldTable->get('players')['data'], true) ?? [];
                 $playersResponseData = [];
                 foreach ($playersData as $playerName => $playerData) {
